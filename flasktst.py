bf3c44bd2f0261f0bd68d795a2388523d14d2660
grant <grantc0493@hotmail.co.uk>
2021-09-27 14:32:46 +0100

    update

--- flasktst.py
+++ flasktst.py
@@ -2,12 +2,20 @@ from flask import Flask, request, render_template
 import subprocess
 
 app = Flask(__name__)
+#wifi = subprocess.check_output(['nmcli', 'dev', 'wifi'])
+#type(wifi)
 
+
+
+ 
 @app.route('/')
 def index():
+    result_success = subprocess.check_output(["ifconfig"], shell=True)
     return render_template('index.html')
 
-@app.route('/script')
+@app.route('/connect')
 def command():
-    result_success = subprocess.check_output(["ifconfig"], shell=True)
-    return result_success
+    ssid = request.args.get("ssid")
+    password = request.args.get("password")
+    #subprocess.check_output([f"nmcli dev wifi connect {ssid} password {password}"], shell=True)
+    return subprocess.check_output(["nmcli", "dev", "wifi", "connect", f"{ssid}", "password" f"{password}"], shell=True)
